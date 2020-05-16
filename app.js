45837cc0a2d3ef3ccefe6eddce6a8507aaa4a1b3
unknown <larrieu.arnaud@gmail.com>
2020-05-16 18:11:08 +0200

    trying to setup a loading page

--- app.js
+++ app.js
@@ -138,3 +138,21 @@ function workPageRedirect() {
     window.location.href = '#work';
   }, 1600);
 }
+
+function onReady(callback) {
+  var intervalId = window.setInterval(function () {
+    if (document.getElementsByTagName('body')[0] !== undefined) {
+      window.clearInterval(intervalId);
+      callback.call(this);
+    }
+  }, 1000);
+}
+
+function setVisible(selector, visible) {
+  document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function () {
+  setVisible('.page', true);
+  setVisible('#loading', false);
+});
