c2deea44b88430a8694b410d94207e7ba499cce3
unknown <larrieu.arnaud@gmail.com>
2020-05-28 00:09:33 +0200

    webrr

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ xmlns="http://www.w3.org/1999/xhtml"
       ></path>
       <text class="rotate-center">
         <textPath href="#MyPath" startOffset="50%" text-anchor="middle">
-          Developpeur Webr
+          Developpeur Webrr
         </textPath>
       </text>
     </svg>
